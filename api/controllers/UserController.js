16900e298baf9c4a1b8d84957cc31082de9c41da
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2014-04-15 22:42:07 -0500

    Adds basic user authentication

---
+++ api/controllers/UserController.js
@@ -0,0 +1,36 @@
+/**
+ * UserController.js 
+ *
+ * @description ::
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+
+  login: function (req, res) {
+    var bcrypt = require('bcrypt');
+
+    User.findOneByEmail(req.body.email).done(function (err, user) {
+      if (err) res.json({ error: 'DB error' }, 500);
+
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, function (err, match) {
+          if (err) res.json({ error: 'Server error' }, 500);
+
+          if (match) {
+            // password match
+            req.session.user = user.id;
+            res.json(user);
+          } else {
+            // invalid password
+            if (req.session.user) req.session.user = null;
+            res.json({ error: 'Invalid password' }, 400);
+          }
+        });
+      } else {
+        res.json({ error: 'User not found' }, 404);
+      }
+    });
+  }	
+
+};
---
+++ api/models/User.js
@@ -0,0 +1,48 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	attributes: {
+    username: {
+      type: 'STRING',
+      unique: true,
+      required: true
+    },
+    email: {
+      type: 'email',
+      unique: true,
+      required: true
+    },
+    password: {
+      type: 'STRING',
+      required: true,
+      minLenght: 6
+    },
+    annotations: {
+      collection: 'annotation',
+      via: 'article'
+    }
+	},
+
+  // Encrypt password before create
+  beforeCreate: function(attrs, next) {
+    var bcrypt = require('bcrypt');
+
+    bcrypt.genSalt(10, function(err, salt){
+      if (err) return next(err);
+
+      bcrypt.hash(attrs.password, salt, function(err, hash){
+        if (err) return next(err);
+
+        attrs.password = hash;
+        next();
+      });
+    });
+  }
+
+};
