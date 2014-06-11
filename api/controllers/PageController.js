d6bb619f157ed4465c4d24fdd4f7b79c2f12a03f
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2014-06-11 18:03:09 -0500

    [#36] dynamic list of laws on landing page

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -12,7 +12,10 @@ module.exports = {
   },
 
   homepage: function(req, res) {
-    res.locals.layout = 'pages/homepageLayout';
-    res.view('pages/homepage')
+    // TODO: sort the laws by more annotations and only return 3 of them
+    Law.find().exec(function(err, laws) {
+      res.locals.layout = 'pages/homepageLayout';
+      res.view('pages/homepage', {laws: laws})
+    });
   }
 };
