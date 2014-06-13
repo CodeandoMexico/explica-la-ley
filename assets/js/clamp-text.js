821a1bb4eb623b4fcc1a866578e11cb2b16d44b7
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2014-06-13 03:02:54 -0600

    Adds article's body preview

--- assets/js/clamp-text.js
+++ assets/js/clamp-text.js
@@ -2,4 +2,7 @@ $(function() {
   $('.clamp-text').each(function(index, element) {
     $clamp(element, {clamp: 5});
   });
+  $('.list-boxes p').each(function(index, element) {
+    $clamp(element, {clamp: 4});
+  });
 });
