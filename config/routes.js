c228ce9935d8a35e42088847edc7535b9f9295ed
Everardo Padilla <everardo.padilla.saca@gmail.com>
2014-09-09 01:00:18 -0500

    Add basic user profile and email saving/forgetting mechanism

--- config/routes.js
+++ config/routes.js
@@ -114,6 +114,21 @@ module.exports.routes = {
     action: 'votedown'
   },
 
+  'get /ley/user': {
+    controller: 'user',
+    action: 'profile'
+  },
+
+  'post /ley/user/saveEmail': {
+    controller: 'user',
+    action: 'saveEmail'
+  },
+
+  'post /ley/user/forgetEmail': {
+    controller: 'user',
+    action: 'forgetEmail'
+  },
+
   // If a request to a URL doesn't match any of the custom routes above, it is matched 
   // against Sails route blueprints.  See `config/blueprints.js` for configuration options
   // and examples.
