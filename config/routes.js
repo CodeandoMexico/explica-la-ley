14b06064b1c1cd161e06a43f3a9ea8af13703bd2
Adrian Rangel <adrian.rangel@gmail.com>
2015-02-10 19:03:16 -0600

    add privacyPolicy

--- config/routes.js
+++ config/routes.js
@@ -24,11 +24,16 @@ module.exports.routes = {
 
   // Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, etc. depending on your
   // default view engine) your home page.
-  // 
+  //
   // (Alternatively, remove this and add an `index.html` file in your `assets` directory)
   '/': {
     controller: 'page',
     action: 'redirect'
+  },
+
+  '/aviso-privacidad': {
+    controller: 'page',
+    action: 'privacyPolicy'
   },
 
   '/admin': {
@@ -229,7 +234,7 @@ module.exports.routes = {
     action: 'forgetEmail'
   },
 
-  // If a request to a URL doesn't match any of the custom routes above, it is matched 
+  // If a request to a URL doesn't match any of the custom routes above, it is matched
   // against Sails route blueprints.  See `config/blueprints.js` for configuration options
   // and examples.
 
